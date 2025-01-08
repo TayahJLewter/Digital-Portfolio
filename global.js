240716337906db14c972c9b31db23c9596f9b914
TayahLewter <162350719+TayahLewter@users.noreply.github.com>
2025-01-07 21:35:47 -0500

    index and contact updates

---
+++ global.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.getElementById("hamburger");
+    const mobileNav = document.getElementById("mobileNav");
+
+    hamburger.addEventListener("click", function () {
+        mobileNav.classList.toggle("show");
+    });
+});
+
